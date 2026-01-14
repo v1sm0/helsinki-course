8965ec707afb6335691b0ffafbd9d850146f503b
v1sm0-ENL <vsoto@enerlink.com>
2026-01-14 17:25:48 -0300

    exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,13 +8,19 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const getStatistics = () => {
+    const total = good + neutral + bad
+    const average = (good - bad) / total || 0
+    const positive = (good / total) * 100 || 0
+    return { total, average, positive }
+  }
 
   return (
     <div>
       <Header text="Give feedback" />
       <CalificationButtons setGood={setGood} setNeutral={setNeutral} setBad={setBad} />
       <Header text="Statistics" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics calculateStatistics={getStatistics()} good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-export default function Statistics({good, neutral, bad}) {
+export default function Statistics({calculateStatistics, good, neutral, bad}) {
+  const { total, average, positive } = calculateStatistics
   return (
     <div>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
