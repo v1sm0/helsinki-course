d8e54da65af01b3569e2319e1174acf0a31dc568
v1sm0-ENL <vsoto@enerlink.com>
2026-01-14 17:46:01 -0300

    exercise 1.11 v2

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -17,12 +17,16 @@ export default function Statistics({good, neutral, bad}) {
 
     return (
       <div>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />  
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={total} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={`${positive} %`} />
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />  
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total} />
+            <StatisticLine text="average" value={average} />
+            <StatisticLine text="positive" value={`${positive} %`} />
+          </tbody>
+        </table>
       </div>
     )
 }
