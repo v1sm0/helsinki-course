d3ff080bc02d15345420a2ae996c5e015b282a95
v1sm0-ENL <vsoto@enerlink.com>
2026-01-14 17:17:04 -0300

    folder reorganization

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+export default App
